7b1dc1e7427e251eea4d4efdfead513a7550c631
Richard Brooks <rickbrooks22@gmail.com>
2019-07-23 10:15:57 -0400

    added while loop

---
+++ whileLoops.py
@@ -0,0 +1,9 @@
+#Creating my first while loop
+
+num_family=4
+
+while num_family > 0:
+    print("I have %s family members living in Atlanta" %num_family)
+    num_family-= 1
+else: 
+    print("Everyone else moved away except for %s family member" %num_family)
