142a18f37a166b0fe461295f56028ec441db690d
Richard Brooks <rickbrooks22@gmail.com>
2019-07-23 10:20:40 -0400

    added second while loop basic syntax

--- whileLoops.py
+++ whileLoops.py
@@ -1,9 +1,15 @@
 #Creating my first while loop
 
 num_family=4
+family_age_sum=185
 
 while num_family > 0:
     print("I have %s family members living in Atlanta" %num_family)
     num_family-= 1
 else: 
     print("Everyone else moved away except for %s family member" %num_family)
+
+#incorporating a break statement
+while family_age_sum < 200:
+    print("Family aged summed is %s "family_age_sum)
+    family_age_sum+= 1
